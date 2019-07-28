343284175e9c63c981dc3e9510d4bbbf71f3e5fb
MaoXianxin <qq1044467857@gmail.com>
2019-07-28 22:03:57 +0800

    add resnet50_distribution

---
+++ official/resnet/keras/resnet50_distribution.py
@@ -0,0 +1,89 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+import resnet_model
+
+import os
+
+datasets, ds_info = tfds.load(name='tf_flowers', with_info=True, as_supervised=True)
+mnist_train, mnist_test = datasets['train'], datasets['train']
+
+strategy = tf.distribute.MirroredStrategy()
+print ('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+
+# You can also do ds_info.splits.total_num_examples to get the total
+# number of examples in the dataset.
+
+num_train_examples = ds_info.splits['train'].num_examples
+num_test_examples = ds_info.splits['train'].num_examples
+
+BUFFER_SIZE = 10000
+
+BATCH_SIZE_PER_REPLICA = 64
+BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
+
+def scale(image, label):
+  image = tf.cast(image, tf.float32)
+  image /= 255
+
+  image = tf.image.resize_images(image, (224,224))
+
+  return image, label
+
+train_dataset = mnist_train.map(scale).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+eval_dataset = mnist_test.map(scale).batch(BATCH_SIZE)
+
+with strategy.scope():
+  # model = tf.keras.Sequential([
+  #     tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
+  #     tf.keras.layers.MaxPooling2D(),
+  #     tf.keras.layers.Flatten(),
+  #     tf.keras.layers.Dense(64, activation='relu'),
+  #     tf.keras.layers.Dense(10, activation='softmax')
+  # ])
+
+  model = resnet_model.resnet50(num_classes=5, batch_size=None)
+
+
+  model.compile(loss='sparse_categorical_crossentropy',
+                optimizer=tf.keras.optimizers.Adam(),
+                metrics=['accuracy'])
+
+# Define the checkpoint directory to store the checkpoints
+
+checkpoint_dir = './training_checkpoints'
+# Name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
+# Function for decaying the learning rate.
+# You can define any decay function you need.
+def decay(epoch):
+  if epoch < 3:
+    return 1e-3
+  elif epoch >= 3 and epoch < 7:
+    return 1e-4
+  else:
+    return 1e-5
+
+# Callback for printing the LR at the end of each epoch.
+class PrintLR(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs=None):
+    print ('\nLearning rate for epoch {} is {}'.format(
+        epoch + 1, tf.keras.backend.get_value(model.optimizer.lr)))
+
+callbacks = [
+    tf.keras.callbacks.TensorBoard(log_dir='./logs'),
+    tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix,
+                                       save_weights_only=True),
+    tf.keras.callbacks.LearningRateScheduler(decay),
+    PrintLR()
+]
+
+model.fit(train_dataset, epochs=50, callbacks=callbacks)
+
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+
+eval_loss, eval_acc = model.evaluate(eval_dataset)
+print ('Eval loss: {}, Eval Accuracy: {}'.format(eval_loss, eval_acc))
